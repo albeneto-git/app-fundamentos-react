bf540f6126a77cdc9663ca2b1fa82353dbe385c2
Albertino Neto <albeneto72@gmail.com>
2026-02-11 08:48:23 -0300

    Exercícios de boas vindas e eventos de botão

---
+++ src/components/Exercises.jsx
@@ -0,0 +1,33 @@
+import {useState} from "react";
+
+// Exercício 1
+const Greeting = ({name}) => {
+    return (<h1>Olá, {name}!</h1>);
+};
+
+// Exercício 2
+const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <p>Vc clicou {count}, vezes no botão!</p>
+            <button onClick={()=>{setCount(count + 1)}}>Clique em mim!</button>
+            <br />
+            <button onClick={()=>{ count > 0 ? setCount(count - 1) : setCount(0)}}>Clique em mim para subtrair!</button>
+        </div>
+    );
+};
+
+const Exercises = () => {
+    return (
+        <div>
+            <h2>Exercício 1</h2>
+            <Greeting name = 'Albertino Neto'/>
+            <h2>Exercício 2</h2>
+            <Counter />
+        </div>
+    );
+};
+
+export default Exercises;
