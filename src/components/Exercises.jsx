edf57b73adb996b5136ff29cd5714332563ffccc
Albertino Neto <albeneto72@gmail.com>
2026-02-11 09:24:12 -0300

    Mostrar listagem a partir de um array de objetos

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -18,13 +18,34 @@ const Counter = () => {
     );
 };
 
-const Exercises = () => {
+// Exercício 3
+const TaskList = ({tasks}) => {
+    if(tasks.length === 0) {
+        return <p>Não há tarefas para mostrar!</p>
+    }
+
+    return(
+        <ul>
+            {tasks.map((task) => (
+                <li key={task.id}>{task.text}</li>
+            ))}
+        </ul>
+    );
+};
+
+function Exercises() {
+    const tasks = [
+        { id: 1, text: "Comprar leite" },
+        { id: 2, text: "Estudar React" },
+    ];
     return (
         <div>
             <h2>Exercício 1</h2>
             <Greeting name = 'Albertino Neto'/>
             <h2>Exercício 2</h2>
             <Counter />
+            <h2>Exercício 3</h2>
+            <TaskList tasks={tasks} />
         </div>
     );
 };
